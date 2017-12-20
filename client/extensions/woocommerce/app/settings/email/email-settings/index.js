a2bb1b2d3fcffe8455a2ce0ebe122e1a17aa4573
budzanowski <bartosz.budzanowski@gmail.com>
2017-12-20 13:43:28 +0100

    Add more tests.

--- client/extensions/woocommerce/app/settings/email/email-settings/index.js
+++ client/extensions/woocommerce/app/settings/email/email-settings/index.js
@@ -1,6 +1,9 @@
 /**
  * External dependencies
+ *
+ * @format
  */
+
 import { bindActionCreators } from 'redux';
 import { translate } from 'i18n-calypso';
 import { get, omit } from 'lodash';
@@ -40,7 +43,7 @@ const originNotifications = [
 		field: 'email',
 		option: 'woocommerce_email_from_name',
 		title: translate( 'From name' ),
-		subtitle: translate( 'Emails will appear in recipients inboxes \'from\' this name.' ),
+		subtitle: translate( "Emails will appear in recipients inboxes 'from' this name." ),
 	},
 	{
 		field: 'email',
@@ -60,12 +63,12 @@ const internalNotifications = [
 	{
 		field: 'email_cancelled_order',
 		title: translate( 'Cancelled order' ),
-		subtitle: translate( 'Sent when a new order is marked \'cancelled\'.' ),
+		subtitle: translate( "Sent when a new order is marked 'cancelled'." ),
 	},
 	{
 		field: 'email_failed_order',
 		title: translate( 'Failed order' ),
-		subtitle: translate( 'Sent when a new order is marked \'failed\'.' ),
+		subtitle: translate( "Sent when a new order is marked 'failed'." ),
 	},
 ];
 
@@ -74,22 +77,22 @@ const customerNotifications = [
 	{
 		field: 'email_customer_on_hold_order',
 		title: translate( 'Order pending payment' ),
-		subtitle: translate( 'Sent when an order is marked \'payment pending\'.' ),
+		subtitle: translate( "Sent when an order is marked 'payment pending'." ),
 	},
 	{
 		field: 'email_customer_processing_order',
 		title: translate( 'Processing order' ),
-		subtitle: translate( 'Sent when an order is marked \'payment processing\'.' ),
+		subtitle: translate( "Sent when an order is marked 'payment processing'." ),
 	},
 	{
 		field: 'email_customer_completed_order',
 		title: translate( 'Completed order' ),
-		subtitle: translate( 'Sent when an order is marked \'paid in full\'.' ),
+		subtitle: translate( "Sent when an order is marked 'paid in full'." ),
 	},
 	{
 		field: 'email_customer_refunded_order',
 		title: translate( 'Refunded order' ),
-		subtitle: translate( 'Sent when an order is marked \'payment refunded\'.' ),
+		subtitle: translate( "Sent when an order is marked 'payment refunded'." ),
 	},
 	{
 		field: 'email_customer_new_account',
@@ -99,11 +102,10 @@ const customerNotifications = [
 ];
 
 class Settings extends React.Component {
-
-	fetchSettings = ( props ) => {
+	fetchSettings = props => {
 		const { siteId, fetchSettings } = props;
 		siteId && fetchSettings( siteId );
-	}
+	};
 
 	componentDidMount = () => {
 		this.fetchSettings( this.props );
@@ -115,83 +117,98 @@ class Settings extends React.Component {
 		}
 
 		// Save settings request
-		if ( ( ! this.props.saveSettingsRequest ) && nextProps.saveSettingsRequest ) {
-			const settingsClean = omit( nextProps.settings, [ 'save', 'isSaving', 'error', 'invalidValue' ] );
+		if ( ! this.props.saveSettingsRequest && nextProps.saveSettingsRequest ) {
+			const settingsClean = omit( nextProps.settings, [
+				'save',
+				'isSaving',
+				'error',
+				'invalidValue',
+			] );
 			const areSettingsValid = validateSettings( settingsClean );
 			if ( nextProps.loaded ) {
 				if ( ! areSettingsValid ) {
-					nextProps.emailSettingsInvalidValue( nextProps.siteId, 'Invalid Values.' );
+					nextProps.emailSettingsInvalidValue(
+						nextProps.siteId,
+						'Email Settings are invalid: please correct.'
+					);
 					nextProps.errorNotice( translate( 'Please correct your Email settings and try again.' ) );
 				} else {
 					nextProps.submit( nextProps.siteId, nextProps.settings );
 				}
 			} else {
-				nextProps.emailSettingsInvalidValue( nextProps.siteId, 'Values not loaded.' );
+				nextProps.emailSettingsInvalidValue(
+					nextProps.siteId,
+					'Email Settings not loaded: please wait.'
+				);
 			}
 		}
 
 		// Settings save request finished.
-		if ( ( ! nextProps.isSaving ) && this.props.isSaving ) {
+		if ( ! nextProps.isSaving && this.props.isSaving ) {
 			if ( nextProps.submitError ) {
-				nextProps.errorNotice( translate( 'There was a problem saving the email settings. Please try again.' ) );
+				nextProps.errorNotice(
+					translate( 'There was a problem saving the email settings. Please try again.' )
+				);
 			} else {
 				nextProps.successNotice( translate( 'Email settings saved.' ), { duration: 4000 } );
 			}
 		}
 	}
 
-	onChange = ( event ) => {
+	onChange = event => {
 		const { onChange, siteId } = this.props;
 		onChange( siteId, event );
-	}
+	};
 
 	renderOriginNotification = ( item, index ) => {
 		const { settings, loading } = this.props;
-		return <NotificationsOrigin
-			key={ index }
-			item={ item }
-			isPlaceholder={ loading }
-			recipient={ get( settings, [ item.field, item.option, 'value' ], '' ) }
-			placeholder={ get( settings, [ item.field, item.option, 'default' ], '' ) }
-			onChange={ this.onChange }
-			checkEmail={ item.checkEmail }
-		/>;
-	}
+		return (
+			<NotificationsOrigin
+				key={ index }
+				item={ item }
+				isPlaceholder={ loading }
+				recipient={ get( settings, [ item.field, item.option, 'value' ], '' ) }
+				placeholder={ get( settings, [ item.field, item.option, 'default' ], '' ) }
+				onChange={ this.onChange }
+				checkEmail={ item.checkEmail }
+			/>
+		);
+	};
 
 	renderInternalNotification = ( item, index ) => {
 		const { settings, loading } = this.props;
-		return <InternalNotification
-			key={ index }
-			item={ item }
-			checked={ 'yes' === get( settings, [ item.field, 'enabled', 'value' ], '' ) }
-			recipient={ get( settings, [ item.field, 'recipient', 'value' ], '' ) }
-			placeholder={ get( settings, [ item.field, 'recipient', 'default' ], '' ) }
-			isPlaceholder={ loading }
-			onChange={ this.onChange }
-		/>;
-	}
+		return (
+			<InternalNotification
+				key={ index }
+				item={ item }
+				checked={ 'yes' === get( settings, [ item.field, 'enabled', 'value' ], '' ) }
+				recipient={ get( settings, [ item.field, 'recipient', 'value' ], '' ) }
+				placeholder={ get( settings, [ item.field, 'recipient', 'default' ], '' ) }
+				isPlaceholder={ loading }
+				onChange={ this.onChange }
+			/>
+		);
+	};
 
 	renderCustomerNotification = ( item, index ) => {
 		const { settings, loading } = this.props;
-		return <CustomerNotification
-			key={ index }
-			item={ item }
-			isPlaceholder={ loading }
-			checked={ 'yes' === get( settings, [ item.field, 'enabled', 'value' ], '' ) }
-			onChange={ this.onChange }
-		/>;
-	}
+		return (
+			<CustomerNotification
+				key={ index }
+				item={ item }
+				isPlaceholder={ loading }
+				checked={ 'yes' === get( settings, [ item.field, 'enabled', 'value' ], '' ) }
+				onChange={ this.onChange }
+			/>
+		);
+	};
 
 	/* eslint-disable wpcalypso/jsx-classname-namespace */
 	render() {
 		return (
 			<div className="email-settings__container">
-				<ExtendedHeader
-					label={ translate( 'Origin' ) }
-				/>
-				<List>
-					{ originNotifications.map( this.renderOriginNotification ) }
-				</List>
+				<ExtendedHeader label={ translate( 'Origin' ) } />
+				<List>{ originNotifications.map( this.renderOriginNotification ) }</List>
 				<div>
 					<ExtendedHeader
 						label={ translate( 'Internal notifications' ) }
@@ -219,12 +236,8 @@ class Settings extends React.Component {
 					/>
 					<List>
 						<ListHeader>
-							<ListItemField>
-								{ translate( 'Email' ) }
-							</ListItemField>
-							<ListItemField>
-								{ translate( 'Enabled' ) }
-							</ListItemField>
+							<ListItemField>{ translate( 'Email' ) }</ListItemField>
+							<ListItemField>{ translate( 'Enabled' ) }</ListItemField>
 						</ListHeader>
 						{ customerNotifications.map( this.renderCustomerNotification ) }
 					</List>
@@ -246,7 +259,8 @@ Settings.propTypes = {
 function mapStateToProps( state, props ) {
 	return {
 		settings: areEmailSettingsLoaded( state, props.siteId )
-			? getEmailSettings( state, props.siteId ) : {},
+			? getEmailSettings( state, props.siteId )
+			: {},
 		loading: areEmailSettingsLoading( state, props.siteId ),
 		loaded: areEmailSettingsLoaded( state, props.siteId ),
 		saveSettingsRequest: emailSettingsSaveRequest( state, props.siteId ),
